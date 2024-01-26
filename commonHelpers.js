3d5839bfd4b9a7487c1c87c799d89efe3c6841f0
FunnyBilbur <tonistark1997@gmail.com>
2024-01-26 11:54:42 +0000

    Deploying to gh-pages from @ FunnyBilbur/project-radioactive@ba6411acf97283d4a007c7dc12ee9c59ffa6bebb 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,9 @@
+import"./assets/main-3cfb730f.js";import{a as s}from"./assets/vendor-0cb09735.js";const o=document.querySelector(".quote-container"),a=new Date().toLocaleDateString();async function c(){o.innerHTML=`
+        <span class="loader"></span>
+        <span>We're looking a quote for you...</span>
+    `;try{return await s.get("https://energyflow.b.goit.study/api/quote").then(t=>t.data)}catch{}}function u(){if(localStorage.getItem("quote")&&localStorage.getItem("savedDate")===a){const{author:e,quote:t}=JSON.parse(localStorage.getItem("quote"));o.innerHTML=n(e,t)}else c().then(({author:e,quote:t})=>{const r={quote:t,author:e};localStorage.setItem("quote",JSON.stringify(r)),localStorage.setItem("savedDate",a),o.innerHTML=n(e,t)}).catch(e=>Notify.failure(e.message))}function n(e,t){return`
+            <h2>Quote of the day</h2>
+            <p><span>${t}</span></p>
+            <p>${e}</p>
+      `}u();
+//# sourceMappingURL=commonHelpers.js.map
