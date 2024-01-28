8c89d3c528cdc51a32b8913936676061fa7d7649
FunnyBilbur <tonistark1997@gmail.com>
2024-01-28 20:01:35 +0000

    Deploying to gh-pages from @ FunnyBilbur/project-radioactive@88607bf27427867852a1b7d5c5216490aab31f5f 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,15 +1,15 @@
-import"./assets/header-f61c3187.js";import{n as l,a as u}from"./assets/vendor-a94eb9aa.js";const n=document.querySelector(".quote-container"),i=new Date().toLocaleDateString();async function p(){n.innerHTML=`
+import"./assets/header-f61c3187.js";import{n as l,a as u}from"./assets/vendor-a94eb9aa.js";const n=document.querySelector(".quote__backend"),i=new Date().toLocaleDateString();async function p(){n.innerHTML=`
         <span class="loader"></span>
         <span>We're looking a quote for you...</span>
-    `;try{return(await u.get("https://energyflow.b.goit.study/api/quote")).data}catch{l.Notify.failure("Something went wrong, try again")}}function c(e,s){return`<p class="quote-text">${s}</p>
-            <p class="quote-author">${e}</p>`}function g(){if(localStorage.getItem("quote")&&localStorage.getItem("savedDate")===i){const{author:e,quote:s}=JSON.parse(localStorage.getItem("quote"));n.innerHTML=c(e,s)}else p().then(({author:e,quote:s})=>{const r={author:e,quote:s};localStorage.setItem("quote",JSON.stringify(r)),localStorage.setItem("savedDate",i),n.innerHTML=c(e,s)}).catch(e=>l.Notify.failure(e.message))}g();document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("subscriptionForm");e.addEventListener("submit",function(s){if(!e.checkValidity())alert("Please fill in a valid email address."),s.preventDefault();else{const r=new FormData(e);fetch(e.action,{method:"POST",body:r}).then(t=>{if(!t.ok)throw new Error("Network response was not ok");return t.json()}).then(t=>{t.message?alert(t.message):console.warn("Response does not contain a message field:",t)}).catch(t=>{console.error("Error:",t)})}})});const d=document.querySelector(".exercises-input-wrapper"),m=d.querySelector(".exercises-search-input"),o=document.querySelector(".exercises-list-page2");d.addEventListener("submit",async e=>{e.preventDefault();const s=m.value.trim();try{await f(s)}catch(r){console.error(r)}});async function f(e){try{const r=(await u.get(`https://energyflow.b.goit.study/api/exercises?bodypart=back&muscles=lats&equipment=barbell&keyword=${e}&page=1&limit=10`)).data;if(o.innerHTML="",r.length>0){const t=r.map(a=>`
+    `;try{return(await u.get("https://energyflow.b.goit.study/api/quote")).data}catch{l.Notify.failure("Something went wrong, try again")}}function c(e,s){return`<p class="quote__backend-text">${s}</p>
+            <p class="quote__backend-author">${e}</p>`}function g(){if(localStorage.getItem("quote")&&localStorage.getItem("savedDate")===i){const{author:e,quote:s}=JSON.parse(localStorage.getItem("quote"));n.innerHTML=c(e,s)}else p().then(({author:e,quote:s})=>{const a={author:e,quote:s};localStorage.setItem("quote",JSON.stringify(a)),localStorage.setItem("savedDate",i),n.innerHTML=c(e,s)}).catch(e=>l.Notify.failure(e.message))}g();document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("subscriptionForm");e.addEventListener("submit",function(s){if(!e.checkValidity())alert("Please fill in a valid email address."),s.preventDefault();else{const a=new FormData(e);fetch(e.action,{method:"POST",body:a}).then(t=>{if(!t.ok)throw new Error("Network response was not ok");return t.json()}).then(t=>{t.message?alert(t.message):console.warn("Response does not contain a message field:",t)}).catch(t=>{console.error("Error:",t)})}})});const d=document.querySelector(".exercises-input-wrapper"),m=d.querySelector(".exercises-search-input"),o=document.querySelector(".exercises-list-page2");d.addEventListener("submit",async e=>{e.preventDefault();const s=m.value.trim();try{await f(s)}catch(a){console.error(a)}});async function f(e){try{const a=(await u.get(`https://energyflow.b.goit.study/api/exercises?bodypart=back&muscles=lats&equipment=barbell&keyword=${e}&page=1&limit=10`)).data;if(o.innerHTML="",a.length>0){const t=a.map(r=>`
         <li class="exercises-item-page2">
           <div class="exercises-card">
             <div class="exercises-card-top">
               <div class="exercises-kind-wrapper">
                 <p class="exercises-card-kind">WORKOUT</p>
                 <div class="exercises-card-rating">
-                  <p class="exercises-rating-value">${a.rating}</p>
+                  <p class="exercises-rating-value">${r.rating}</p>
                   <svg class="exercises-star-icon" width="18" height="18" aria-label="star icon">
                     <use href="./img/exercises/exercises-sprite.svg#icon-star"></use>
                   </svg>
@@ -26,17 +26,17 @@ import"./assets/header-f61c3187.js";import{n as l,a as u}from"./assets/vendor-a9
               <svg class="exercises-star-icon" width="24" height="24" aria-label="star icon">
                 <use href="./img/exercises/exercises-sprite.svg#icon-icon"></use>
               </svg>
-              <p class="exercises-card-exname">${a.name}</p>
+              <p class="exercises-card-exname">${r.name}</p>
             </div>
             <ul class="exercises-card-info">
               <li class="exercises-info-data">
-                <span class="exercises-data-name">Burned calories: </span>${a.calories} / ${a.duration} min
+                <span class="exercises-data-name">Burned calories: </span>${r.calories} / ${r.duration} min
               </li>
               <li class="exercises-info-data">
-                <span class="exercises-data-name">Body part: </span>${a.bodyPart}
+                <span class="exercises-data-name">Body part: </span>${r.bodyPart}
               </li>
               <li class="exercises-info-data">
-                <span class="exercises-data-name">Target: </span>${a.targetMuscle}
+                <span class="exercises-data-name">Target: </span>${r.targetMuscle}
               </li>
             </ul>
           </div>
