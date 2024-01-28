314aad7ab60d7efe2eb81c15606bee55e8d19979
FunnyBilbur <tonistark1997@gmail.com>
2024-01-28 21:08:53 +0000

    Deploying to gh-pages from @ FunnyBilbur/project-radioactive@8ddf67356736b6f38596b5eb10e18eeee07cecf2 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -2,14 +2,14 @@ import"./assets/header-b6fb7402.js";import{n as l,a as d}from"./assets/vendor-a9
         <span class="loader"></span>
         <span>We're looking a quote for you...</span>
     `;try{return(await d.get("https://energyflow.b.goit.study/api/quote")).data}catch{l.Notify.failure("Something went wrong, try again")}}function c(e,s){return`<p class="quote__backend-text">${s}</p>
-            <p class="quote__backend-author">${e}</p>`}function g(){if(localStorage.getItem("quote")&&localStorage.getItem("savedDate")===i){const{author:e,quote:s}=JSON.parse(localStorage.getItem("quote"));n.innerHTML=c(e,s)}else p().then(({author:e,quote:s})=>{const a={author:e,quote:s};localStorage.setItem("quote",JSON.stringify(a)),localStorage.setItem("savedDate",i),n.innerHTML=c(e,s)}).catch(e=>l.Notify.failure(e.message))}g();document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("subscriptionForm");e.addEventListener("submit",function(s){if(!e.checkValidity())alert("Please fill in a valid email address."),s.preventDefault();else{const a=new FormData(e);fetch(e.action,{method:"POST",body:a}).then(t=>{if(!t.ok)throw new Error("Network response was not ok");return t.json()}).then(t=>{t.message?alert(t.message):console.warn("Response does not contain a message field:",t)}).catch(t=>{console.error("Error:",t)})}})});const u=document.getElementById("exercises-search-form"),m=u.querySelector(".exercises-search-input"),o=document.querySelector(".exercises-list-page2");u.addEventListener("submit",async e=>{e.preventDefault();const s=m.value.trim().toLowerCase();try{await f(s)}catch(a){console.error(a)}});async function f(e){try{const a=(await d.get(`https://energyflow.b.goit.study/api/exercises?bodypart=back&muscles=lats&equipment=barbell&keyword=${e}&page=1&limit=10`)).data;if(o.innerHTML="",a.length>0){const t=a.map(r=>`
+            <p class="quote__backend-author">${e}</p>`}function g(){if(localStorage.getItem("quote")&&localStorage.getItem("savedDate")===i){const{author:e,quote:s}=JSON.parse(localStorage.getItem("quote"));n.innerHTML=c(e,s)}else p().then(({author:e,quote:s})=>{const t={author:e,quote:s};localStorage.setItem("quote",JSON.stringify(t)),localStorage.setItem("savedDate",i),n.innerHTML=c(e,s)}).catch(e=>l.Notify.failure(e.message))}g();document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("subscriptionForm");e.addEventListener("submit",function(s){if(s.preventDefault(),!e.checkValidity())alert("Please fill in a valid email address.");else{const t=new FormData(e);fetch("https://energyflow.b.goit.study/api-docs/#/Subscriptions",{method:"POST",body:t}).then(r=>{if(!r.ok)throw new Error("Network response was not ok");return r.json()}).then(r=>{alert("We're excited to have you on board! 🎉 Thank you for subscribing to new exercises on Energy Flow. You've just taken a significant step towards improving your fitness and well-being."),console.log("Server response:",r)}).catch(r=>{console.error("Error:",r)})}})});const u=document.getElementById("exercises-search-form"),m=u.querySelector(".exercises-search-input"),o=document.querySelector(".exercises-list-page2");u.addEventListener("submit",async e=>{e.preventDefault();const s=m.value.trim().toLowerCase();try{await f(s)}catch(t){console.error(t)}});async function f(e){try{const t=(await d.get(`https://energyflow.b.goit.study/api/exercises?bodypart=back&muscles=lats&equipment=barbell&keyword=${e}&page=1&limit=10`)).data;if(o.innerHTML="",t.length>0){const r=t.map(a=>`
         <li class="exercises-item-page2">
           <div class="exercises-card">
             <div class="exercises-card-top">
               <div class="exercises-kind-wrapper">
                 <p class="exercises-card-kind">WORKOUT</p>
                 <div class="exercises-card-rating">
-                  <p class="exercises-rating-value">${r.rating}</p>
+                  <p class="exercises-rating-value">${a.rating}</p>
                   <svg class="exercises-star-icon" width="18" height="18" aria-label="star icon">
                     <use href="./img/exercises/exercises-sprite.svg#icon-star"></use>
                   </svg>
@@ -26,20 +26,20 @@ import"./assets/header-b6fb7402.js";import{n as l,a as d}from"./assets/vendor-a9
               <svg class="exercises-star-icon" width="24" height="24" aria-label="star icon">
                 <use href="./img/exercises/exercises-sprite.svg#icon-icon"></use>
               </svg>
-              <p class="exercises-card-exname">${r.name}</p>
+              <p class="exercises-card-exname">${a.name}</p>
             </div>
             <ul class="exercises-card-info">
               <li class="exercises-info-data">
-                <span class="exercises-data-name">Burned calories: </span>${r.calories} / ${r.duration} min
+                <span class="exercises-data-name">Burned calories: </span>${a.calories} / ${a.duration} min
               </li>
               <li class="exercises-info-data">
-                <span class="exercises-data-name">Body part: </span>${r.bodyPart}
+                <span class="exercises-data-name">Body part: </span>${a.bodyPart}
               </li>
               <li class="exercises-info-data">
-                <span class="exercises-data-name">Target: </span>${r.targetMuscle}
+                <span class="exercises-data-name">Target: </span>${a.targetMuscle}
               </li>
             </ul>
           </div>
         </li>
-      `).join("");o.innerHTML=t}else o.innerHTML='<p class="no-results-message">Unfortunately, <span class="error-message">no results</span> were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</p>'}catch(s){console.error(s)}}
+      `).join("");o.innerHTML=r}else o.innerHTML='<p class="no-results-message">Unfortunately, <span class="error-message">no results</span> were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</p>'}catch(s){console.error(s)}}
 //# sourceMappingURL=commonHelpers2.js.map
